12c3cef7f4bc760bdd5c436e1d96fca37af6b1ae
JineshPrajapat <jineshprajapat585@gmail.com>
2024-06-21 19:31:54 +0530

    minor changes

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,3 +1,3 @@
-// const baseURL = "https://ctae-website-fzxz.vercel.app/api/v1";
-const baseURL = "http://localhost:5000/api/v1";
+const baseURL = "https://ctae-website-fzxz.vercel.app/api/v1";
+// const baseURL = "http://localhost:5000/api/v1";
 export default baseURL;
